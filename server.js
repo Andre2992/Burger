d410474e829d37bfef652e32823b829cd94cea71
andre2992 <andrebrown2929@gmail.com>
2018-09-05 21:40:56 -0400

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require ('method-override');
 var exphbs = require ('express-handlebars');
 
 var app = express();
-app.use (express.static(__dirname + 'public'));
+app.use (express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -18,7 +18,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine','handlebars');
 
 var routes = require ('./controllers/routes.js');
-app.use('/',routes)
+app.use('/',routes);
 
 var port = 3000;
 app.listen(port);
